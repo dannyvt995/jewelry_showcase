5538e7e5c7e35c41cc4aa7ab01e379e8ae246499
tuananhnvl <creatiflow.danny@gmail.com>
2024-12-04 18:20:34 +0700

    ui

--- src/jewelryModuleProgress/ViewDrei/index.js
+++ src/jewelryModuleProgress/ViewDrei/index.js
@@ -5,35 +5,37 @@ import { ViewBuffer, ViewEffectsWrapper, ViewPostProcessing } from "../../utils/
 import { BlendFunction } from 'postprocessing'
 import LoadControls from '../LoadControls'
 import SceneWrapper from '../LoadControls'
+
 import s from './style.module.css'
 import { memo } from "react";
+import { View } from "@react-three/drei";
 function ViewDrei({ modelId }) {
-    return (
-        <ViewEffectsWrapper className={s.view} /* dpr={[1, 2]} */>
-
-            <LoadControls />
-            <LoadModel modelId={modelId} />
-            <ViewPostProcessing multisampling={1} stencilBuffer={false} depthBuffer={false} disableNormalPass={false}>
-                {/*  <Noise opacity={0.75} blendFunction={BlendFunction.OVERLAY} /> */}
-                {/* <ChromaticAberration radialModulation={true} modulationOffset={0.4} offset={[0.01, 0.01]} /> */}
-                <Bloom mipmapBlur levels={2} luminanceThreshold={.8} intensity={.5} />
-                {/*   <N8AO color="black" aoRadius={12} intensity={50.15} /> */}
-                {/*      <SSAO
-blendFunction={BlendFunction.MULTIPLY} // blend mode
-samples={30} // amount of samples per pixel (shouldn't be a multiple of the ring count)
-rings={4} // amount of rings in the occlusion sampling pattern
-distanceThreshold={1.0} // global distance threshold at which the occlusion effect starts to fade out. min: 0, max: 1
-distanceFalloff={0.0} // distance falloff. min: 0, max: 1
-rangeThreshold={0.5} // local occlusion range threshold at which the occlusion starts to fade out. min: 0, max: 1
-rangeFalloff={0.1} // occlusion range falloff. min: 0, max: 1
-luminanceInfluence={0.9} // how much the luminance of the scene influences the ambient occlusion
-radius={20} // occlusion sampling radius
-scale={0.5} // scale of the ambient occlusion
-bias={0.5} // occlusion bias
-/> */}
-            </ViewPostProcessing>
-
-        </ViewEffectsWrapper>
-    )
+    if(1===1) {
+        return (
+            <ViewEffectsWrapper className={s.view} /* dpr={[1, 2]} */>
+    
+                <LoadControls />
+                <LoadModel modelId={modelId} />
+                <ViewPostProcessing multisampling={1} stencilBuffer={false} depthBuffer={false} disableNormalPass={false}>
+                    {/*  <Noise opacity={0.75} blendFunction={BlendFunction.OVERLAY} /> */}
+                    {/* <ChromaticAberration radialModulation={true} modulationOffset={0.4} offset={[0.01, 0.01]} /> */}
+                   {/*  <Bloom mipmapBlur levels={2} luminanceThreshold={.8} intensity={.5} /> */}
+                    {/*   <N8AO color="black" aoRadius={12} intensity={50.15} /> */}
+                </ViewPostProcessing>
+    
+            </ViewEffectsWrapper>
+        )
+    }else{
+        return (
+            <View className={s.view}>
+    
+                <LoadControls />
+                <LoadModel modelId={modelId} />
+             
+    
+            </View>
+        )
+    }
+   
 }
 export default  memo(ViewDrei)
